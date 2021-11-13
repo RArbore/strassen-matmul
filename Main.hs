161a6fd0878cf793766f23b22fc75b80c91b6b32
Russel Arbore <russel.jma@gmail.com>
2021-11-12 19:38:11 -0600

    Matrix type, const init, diagonalize list

--- Main.hs
+++ Main.hs
@@ -10,6 +10,20 @@
     You should have received a copy of the GNU General Public License
     along with strassen-matmul. If not, see <https://www.gnu.org/licenses/>.  -}
 
+data Matrix a = Matrix { matData :: [[a]],
+                         rows :: Int,
+                         cols :: Int } deriving (Show)
+
+matInit :: (Num a) => a -> Int -> Int -> Matrix a
+matInit x r c = Matrix (take r $ repeat $ take c $ repeat x) r c
+
+matDiag :: (Num a) => [a] -> Matrix a
+matDiag [] = Matrix [] 0 0
+matDiag (x:xs) = Matrix ((x:(take (length xs) $ repeat 0)):(map ((:) 0) (matData $ matDiag xs))) r c
+  where r = length xs + 1
+        c = r
+
 main :: IO ()
 main = do
-  print "Hello world."
+  print $ matInit (1.7 :: Float) 3 3
+  print $ matDiag ([1.0, 2.0, 3.0] :: [Float])
